09a691c35a956fb4950a30d1c73f6a9d3390c1bc
7y1o <tdesu.webdesign@gmail.com>
2022-06-15 19:34:50 +0300

    Created CreationPanel component

---
+++ src/components/editor/CreationPanel.vue
@@ -0,0 +1,11 @@
+<template>
+    <it-tabs>
+        <it-tab title="Все"></it-tab>
+        <it-tab title="Примитивы"></it-tab>
+        <it-tab title="Эффекты"></it-tab>
+        <it-tab title="Триггеры"></it-tab>
+    </it-tabs>
+</template>
+
+<script lang="ts" src="./script/creation-panel.ts"></script>
+<style lang="scss" src="./style/creation-panel.scss"></style>
