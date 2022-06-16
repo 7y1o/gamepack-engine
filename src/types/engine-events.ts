70a3e67bd14dcbec0aac1e59b80ef265ec23b4f6
7y1o <tdesu.webdesign@gmail.com>
2022-06-16 12:31:16 +0300

    Added new engine events

--- src/types/engine-events.ts
+++ src/types/engine-events.ts
@@ -2,7 +2,7 @@
 /** Settings update data */
 export interface IEngineSettingsUpdate {
     theme?: 'light',
-    primaryColor: 'red' | 'orange' | 'green' | 'blue' | 'purple' | 'magenta',
+    primaryColor?: 'red' | 'orange' | 'green' | 'blue' | 'purple' | 'magenta',
     editor?: {
         panels?: {
             creation?: {
@@ -11,7 +11,7 @@ export interface IEngineSettingsUpdate {
                 tabPrimitivesEnabled?: boolean,
                 tabFxEnabled?: boolean,
                 tabTriggersEnabled?: boolean,
-                tabPositions: ('all' | 'primitives' | 'fx' | 'triggers')[]
+                tabPositions?: ('all' | 'primitives' | 'fx' | 'triggers')[]
             },
             inspector?: {
                 width?: number,
@@ -28,4 +28,34 @@ export interface IEngineSettingsUpdate {
         gpuMemStatsDisplayMode?: 'all' | 'bar' | 'text'
     },
 
+}
+
+/** Settings interface */
+export interface IEngineSettings {
+    theme: 'light',
+    primaryColor: 'red' | 'orange' | 'green' | 'blue' | 'purple' | 'magenta',
+    editor: {
+        panels: {
+            creation: {
+                width: number,
+                tabAllEnabled: boolean,
+                tabPrimitivesEnabled: boolean,
+                tabFxEnabled: boolean,
+                tabTriggersEnabled: boolean,
+                tabPositions: ('all' | 'primitives' | 'fx' | 'triggers')[]
+            },
+            inspector: {
+                width: number,
+            }
+        },
+    },
+    statusBar: {
+        ramStatsEnabled: boolean,
+        gpuCoreStatsEnabled: boolean,
+        gpuMemStatsEnabled: boolean,
+        fpsStatsEnabled: boolean,
+        
+        ramStatsDisplayMode: 'all' | 'bar' | 'text',
+        gpuMemStatsDisplayMode: 'all' | 'bar' | 'text'
+    },
 }
--- src/utils/engine-globals-register.ts
+++ src/utils/engine-globals-register.ts
@@ -3,10 +3,9 @@ import bus from './event-bus';
 
 /** 
  * Register global engine events
- * Engine events: /docs/engine-events.md 
  */
 export default function registerGlobalEngineEvents() {
-    bus.on('engine:settings:update', (data: IEngineSettingsUpdate) => {
-        console.log('It works!')
-    });
+    bus.on('engine:settings:update', (data: IEngineSettingsUpdate) => {});
+    bus.on('engine:settings:load', (data: IEngineSettingsUpdate) => {});
+    bus.on('engine:settings:reset', (data: IEngineSettingsUpdate) => {});
 }
